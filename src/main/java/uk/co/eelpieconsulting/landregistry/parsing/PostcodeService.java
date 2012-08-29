4ed9487f0657ac667f86ea21df157ad4d0877d15
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-08-29 23:10:00 +0100

    Logging and postcode api config.

--- src/main/java/uk/co/eelpieconsulting/landregistry/parsing/PostcodeService.java
+++ src/main/java/uk/co/eelpieconsulting/landregistry/parsing/PostcodeService.java
@@ -1,7 +1,9 @@
 package uk.co.eelpieconsulting.landregistry.parsing;
 
+import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import uk.co.eelpieconsulting.common.geo.LatLong;
@@ -10,9 +12,14 @@ import uk.co.eelpieconsulting.common.http.HttpFetcher;
 
 @Component
 public class PostcodeService {
-
+	
+	private final static Logger log = Logger.getLogger(PostcodeService.class);
+	
 	private HttpFetcher httpFetcher;
-
+	
+	@Value("#{landRegistry['postcodeApiUrl']}")
+	private String apiUrl;
+	
 	public PostcodeService() {
 		httpFetcher = new HttpFetcher();
 	}
@@ -20,17 +27,15 @@ public class PostcodeService {
 	public LatLong getLatLongFor(String postcode) {
 		final String id = postcode.replaceAll(" ", "");
 		try {
-			final String json = httpFetcher.fetchContent("http://localhost:8080/postcodes-api-1.0/postcode/" + id);			
+			final String json = httpFetcher.fetchContent(apiUrl + "/postcode/" + id);			
 			final JSONObject jsonObject = new JSONObject(json);
 			return new LatLong(jsonObject.getDouble("latitude"), jsonObject.getDouble("longitude"));
 			
 		} catch (HttpFetchException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error(e);
 
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error(e);
 		}
 		
 		return null;
