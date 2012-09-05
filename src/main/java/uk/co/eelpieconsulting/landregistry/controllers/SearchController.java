7c6d310458bfc873eefad421b822597164ea5d7e
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-09-05 19:42:19 +0100

    Output source file information on /sources

--- src/main/java/uk/co/eelpieconsulting/landregistry/controllers/SearchController.java
+++ src/main/java/uk/co/eelpieconsulting/landregistry/controllers/SearchController.java
@@ -10,26 +10,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
+import uk.co.eelpieconsulting.common.files.FileInformationService;
 import uk.co.eelpieconsulting.common.views.ViewFactory;
 import uk.co.eelpieconsulting.landregistry.daos.PricePaidDAO;
+import uk.co.eelpieconsulting.landregistry.parsing.PricePaidFileFinder;
 
 @Controller
 public class SearchController {
 	
 	private final PricePaidDAO pricePaidDAO;
+	private PricePaidFileFinder pricePaidFileFinder;
 	private final ViewFactory viewFactory;
+	private FileInformationService fileInformationService;
 	
 	@Autowired
-	public SearchController(PricePaidDAO pricePaidDAO, ViewFactory viewFactory) {
+	public SearchController(PricePaidDAO pricePaidDAO, PricePaidFileFinder pricePaidFileFinder, ViewFactory viewFactory) {
 		this.pricePaidDAO = pricePaidDAO;
+		this.pricePaidFileFinder = pricePaidFileFinder;
 		this.viewFactory = viewFactory;
-	}
-	
-	@RequestMapping("/search")
-	public ModelAndView search() throws IOException, ParseException  {
-		final ModelAndView mv = new ModelAndView(viewFactory.getJsonView());		
-		mv.addObject("data", pricePaidDAO.getAll());
-		return mv;
+		fileInformationService = new FileInformationService();
 	}
 	
 	@RequestMapping("/near")
@@ -44,6 +43,13 @@ public class SearchController {
 		return mv;
 	}
 	
+	@RequestMapping("/sources")
+	public ModelAndView sources() throws IOException, ParseException  {
+		final ModelAndView mv = new ModelAndView(viewFactory.getJsonView());		
+		mv.addObject("data", fileInformationService.makeFileInformationForFiles(pricePaidFileFinder.getFilesInAscendingOrder()));
+		return mv;
+	}
+		
 	private View getRssViewFor(double latitude, double longitude) {
 		final String title = "Prices paid near " + latitude + ", " + longitude;
 		return viewFactory.getRssView(title, "http://www.landregistry.gov.uk/public/information/public-data/price-paid-data", title);
