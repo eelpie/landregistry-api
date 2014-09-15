d52def379a3d1ef7768a30c79e0a57e018f83237
tony <tony@eelpieconsulting.co.uk>
2014-09-15 10:42:56 +0100

    Deduplicate snapshots.

--- src/main/java/uk/co/eelpieconsulting/landregistry/controllers/ZooplaController.java
+++ src/main/java/uk/co/eelpieconsulting/landregistry/controllers/ZooplaController.java
@@ -3,6 +3,7 @@ package uk.co.eelpieconsulting.landregistry.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,18 +46,27 @@ public class ZooplaController {
 	}
 	
 	private Map<String, List<Listing>> groupSnapshotsByListingId(List<Listing> snapshots) {
-		Map<String, List<Listing>> grouped = Maps.newHashMap();
+		Map<String, Map<String, Listing>> grouped = Maps.newHashMap();
 		for (Listing listing : snapshots) {
 			final String listingId = listing.getId().split("-")[0];
 			
-			List<Listing> listingSnapshots = grouped.get(listingId);
+			Map<String, Listing> listingSnapshots = grouped.get(listingId);
 			if (listingSnapshots == null) {
-				listingSnapshots = Lists.newArrayList();
+				listingSnapshots = Maps.newHashMap();
 			}
-			listingSnapshots.add(listing);
+			
+			final String checksum = DigestUtils.md5Hex(listing.getDescription() + listing.getListing_status() + listing.getPrice());			
+			listingSnapshots.put(checksum, listing);
+			
 			grouped.put(listingId, listingSnapshots);
 		}
-		return grouped;
+		
+		final Map<String, List<Listing>> deduplicated = Maps.newHashMap();
+		for (String listingId : grouped.keySet()) {
+			deduplicated.put(listingId, Lists.newArrayList(grouped.get(listingId).values()));
+		}
+				
+		return deduplicated;
 	}
 	
 }
