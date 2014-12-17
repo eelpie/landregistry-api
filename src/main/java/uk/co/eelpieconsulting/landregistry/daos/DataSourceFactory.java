52550c2d79a41e1d69bcecfd90925ac83207a0e5
tony <tony@eelpieconsulting.co.uk>
2014-12-17 16:38:38 +0000

    Make mongo connect and ensure index block in the constructor.
    Suspected multiple index calls from use of the DOAs on start up.

--- src/main/java/uk/co/eelpieconsulting/landregistry/daos/DataSourceFactory.java
+++ src/main/java/uk/co/eelpieconsulting/landregistry/daos/DataSourceFactory.java
@@ -2,6 +2,8 @@ package uk.co.eelpieconsulting.landregistry.daos;
 
 import java.net.UnknownHostException;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,28 +18,36 @@ import com.mongodb.MongoException;
 @Component
 public class DataSourceFactory {
 	
-	@Value("${mongoHost}")
-    private String mongoHost;
-    
-	@Value("${mongoDatabase}")
-    private String mongoDatabase;
-        
-	public DataSourceFactory() {
-	}
-
-	public DataSourceFactory(String mongoHost, String mongoDatabase) {
-		this.mongoHost = mongoHost;
-		this.mongoDatabase = mongoDatabase;
+	private final static Logger log = Logger.getLogger(DataSourceFactory.class);
+	
+	private final Datastore datastore;
+	
+	@Autowired
+	public DataSourceFactory(@Value("${mongoHost}") String mongoHost, @Value("${mongoDatabase}") String mongoDatabase) throws UnknownHostException {
+		this.datastore = connect(mongoHost,  mongoDatabase);
 	}
 	
 	public Datastore getDatastore() throws UnknownHostException, MongoException {	
+		return datastore;
+	}
+
+	private Datastore connect(String mongoHost, String mongoDatabase) throws UnknownHostException {
 		Morphia morphia = new Morphia();
 		morphia.getMapper().getConverters().addConverter(BigDecimalConvertor.class);
 		
+		log.info("Connecting to mongo host: " + mongoHost);
 		Mongo m = new Mongo(mongoHost);
+		
+		log.info("Creating datastore with database: " + mongoDatabase);
 		final Datastore dataStore = morphia.createDatastore(m, mongoDatabase);
+		
+		log.info("Mapping classes");
 		morphia.map(PricePaid.class);
+		
+		log.info("Ensuring indexes");
 		dataStore.ensureIndexes();
+		
+		log.info("Returning datastore");
 		return dataStore;
 	}
 	
